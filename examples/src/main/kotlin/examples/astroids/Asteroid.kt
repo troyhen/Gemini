d7370c8916c4bfda51b790a203fd0b381ad3a77a
Troy <troyhen@churchofjesuschrist.org>
2023-10-02 18:24:15 -0600

    Make Asteroids rough and have spin

--- examples/src/main/kotlin/examples/astroids/Asteroid.kt
+++ examples/src/main/kotlin/examples/astroids/Asteroid.kt
@@ -3,6 +3,7 @@ package examples.astroids
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import gemini.engine.Collider
 import gemini.engine.SceneScope
@@ -10,16 +11,38 @@ import gemini.engine.Stage
 import gemini.foundation.MovingThing
 import gemini.geometry.*
 import gemini.random
+import gemini.rotate
+import gemini.toOffset
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
 import kotlin.time.Duration
 
+
 class Asteroid(
     position: Position,
     velocity: Velocity,
-    private val size: Int = 4,
-) : MovingThing(position, velocity), Collider {
+    spin: Angle,
+    private val baseSize: Int = 4,
+) : MovingThing(position, velocity, spin), Collider {
+
+    private val path: Path by lazy {
+        val center = (position.size / 2f).toOffset()
+        val s2 = center.x / 2
+        Path().apply {
+            val step = 360f / 12
+            repeat(12) {
+                val distance = s2.random + s2
+                val angle = (it * step).degrees
+                val offset = center + Offset(distance, 0f).rotate(angle)
+                if (it == 0) {
+                    moveTo(offset.x, offset.y)
+                } else {
+                    lineTo(offset.x, offset.y)
+                }
+            }
+        }
+    }
 
     override suspend fun act(elapsed: Duration) {
         super.act(elapsed)
@@ -39,36 +62,39 @@ class Asteroid(
         return false
     }
 
+    override fun DrawScope.draw() {
+        drawPath(path, Color.Gray)
+    }
+
     fun explode() {
         Stage.instance?.run {
             remove(this@Asteroid)
-            if (size <= 1) return
+            if (baseSize <= 1) return
             add(
                 Asteroid(
                     position = Position(position.location + Offset(position.size.width, position.size.height).random, position.size / 2f),
                     velocity = velocity * 1.5f + Offset(20f, 20f).random,
-                    size = size / 2
+                    spin = spin + 10.degrees.random - 5.degrees,
+                    baseSize = baseSize / 2,
                 )
             )
             add(
                 Asteroid(
                     position = Position(position.location + Offset(position.size.width, position.size.height).random, position.size / 2f),
                     velocity = velocity * 1.5f + Offset(20f, 20f).random,
-                    size = size / 2
+                    spin = spin + 10.degrees.random - 5.degrees,
+                    baseSize = baseSize / 2,
                 )
             )
             add(
                 Asteroid(
                     position = Position(position.location + Offset(position.size.width, position.size.height).random, position.size / 2f),
                     velocity = velocity * 1.5f + Offset(20f, 20f).random,
-                    size = size / 2
+                    spin = spin + 10.degrees.random - 5.degrees,
+                    baseSize = baseSize / 2,
                 )
             )
         }
-    }
-
-    override fun DrawScope.draw() {
-        drawOval(Color.Gray, Offset.Zero, position.size)
     }
 }
 
@@ -79,7 +105,8 @@ fun SceneScope.asteroid(screenSize: Size, size: Int = 4): Asteroid {
     val location = Location(screenSize.width.random, screenSize.height.random)
     val direction = PI2.random
     val velocity = Velocity(speed * cos(direction), speed * sin(direction))
-    return Asteroid(Position(location, Size(diameter, diameter)), velocity).also {
+    val spin = 30.degrees.random - 15.degrees
+    return Asteroid(Position(location, Size(diameter, diameter)), velocity, spin).also {
         add(it)
     }
 }
--- gemini/src/main/kotlin/gemini/Ext.kt
+++ gemini/src/main/kotlin/gemini/Ext.kt
@@ -2,7 +2,11 @@ package gemini
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import gemini.geometry.Angle
 import gemini.geometry.Scale
+import gemini.geometry.degrees
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
 import kotlin.time.Duration
 
@@ -32,11 +36,18 @@ inline fun <E> List<E>.fastForEachIndexed(block: (Int, E) -> Unit) {
 
 val Duration.inSeconds: Float get() = inWholeMicroseconds * 1e-6f
 
+val Angle.random: Angle get() = (Random.nextFloat() * degrees).degrees
 val Double.random: Double get() = Random.nextDouble() * this
 val Float.random: Float get() = Random.nextFloat() * this
 val Int.random: Int get() = Random.nextInt(this)
 val Long.random: Long get() = Random.nextLong(this)
 val Offset.random: Offset get() = Offset(x.random, y.random)
 val Size.random: Size get() = Size(width.random, height.random)
+fun Offset.rotate(angle: Angle): Offset {
+    val c = cos(angle.radians)
+    val s = sin(angle.radians)
+    return Offset(x * c - y * s, y * c + x * s)
+}
 
 operator fun Size.times(scale: Scale): Size = Size(width * scale.x, height * scale.y)
+fun Size.toOffset(): Offset = Offset(width, height)
