c4e516439079634fa8a11f989618174a0bef1066
Troy <troyhen@churchofjesuschrist.org>
2023-10-04 19:55:44 -0600

    Simplify by removing unneeded code

--- examples/src/main/kotlin/examples/starfield/Starfield.kt
+++ examples/src/main/kotlin/examples/starfield/Starfield.kt
@@ -1,21 +1,16 @@
 package examples.starfield
 
 import androidx.compose.foundation.focusable
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.key.*
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.isPrimaryPressed
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.window.singleWindowApplication
 import gemini.*
 import gemini.engine.Gemini
@@ -37,7 +32,6 @@ private class Star : MovingThing() {
     }
 
     override fun DrawScope.orientAndDraw() {
-//        val depth = size.min * 4
         val z = position.location.z.coerceAtLeast(MIN_Z)
         val offset = (position.location.offset / z).remap(size)
         val r0 = size.min / (z * 100)
@@ -66,43 +60,34 @@ fun SceneScope.starField(number: Int) {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun StarField(modifier: Modifier = Modifier) {
     val keys = remember { mutableSetOf<Key>() }
-    val pressed = remember { mutableSetOf<Int>() }
     val stars = rememberScene {
         background(Color.Black)
         starField(100)
         frameRate(Color.White)
     }
-    BoxWithConstraints(modifier.fillMaxSize()) {
-        val requester = remember { FocusRequester() }
-        Gemini(modifier.fillMaxSize()
-            .focusRequester(requester)
-            .focusable()
-            .onKeyEvent {
-                when (it.type) {
-                    KeyEventType.KeyDown -> {
-                        keys.add(it.key)
-                        detectKeys(keys)
-                    }
-                    KeyEventType.KeyUp -> keys.remove(it.key)
-                    else -> Unit
+    val requester = remember { FocusRequester() }
+    Gemini(modifier.fillMaxSize()
+        .focusRequester(requester)
+        .focusable()
+        .onKeyEvent {
+            when (it.type) {
+                KeyEventType.KeyDown -> {
+                    keys.add(it.key)
+                    detectKeys(keys)
                 }
-                true
-            }.onPointerEvent(PointerEventType.Press) {
-                if (it.buttons.isPrimaryPressed) pressed.add(0)
-//                println(pressed)
-            }.onPointerEvent(PointerEventType.Release) {
-                if (!it.buttons.isPrimaryPressed) pressed.remove(0)
-//                println(pressed)
-            }) {
-            scene = stars
-        }
-        LaunchedEffect(requester) {
-            requester.requestFocus()
-        }
+
+                KeyEventType.KeyUp -> keys.remove(it.key)
+                else -> Unit
+            }
+            true
+        }) {
+        scene = stars
+    }
+    LaunchedEffect(requester) {
+        requester.requestFocus()
     }
 }
 
